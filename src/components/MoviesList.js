8127c2e97684f120ddc6623b9650b1526dccb395
ajlic24@gmail.com <ajlic24@gmail.com>
2022-02-05 13:23:00 -0500

    done

---
+++ src/components/MoviesList.js
@@ -0,0 +1,12 @@
+import {Link} from 'react-router-dom'
+
+function MoviesList({ movies }) {
+    return (
+        <ul>{Object.keys(movies).map(movieID => {
+            return <li key={movieID} ><Link to={`/movies/${movieID}`} >{movies[movieID].title}</Link></li>
+        })}
+        </ul>
+    )
+}
+
+export default MoviesList
